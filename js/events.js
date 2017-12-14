02c9df47c9db9403889a5ba181be30c047b223b9
Jose Luis Jardon <jljardon.c@gmail.com>
2017-12-14 05:45:05 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -9,31 +9,11 @@ function frameIt(){
 }
 
 
-function pressIt(){
-   var pressed = 0;
-   $('#typing').on('keydown', function(event) {
-    if(event.which == 71){
-      alert('You pressed the G key!');
-      //pressed = 71;
-    }
-   });
-/*   if(pressed = 71){
 
-      return;
-   }*/
-}
-
-function submitIt(){
-  if ($( "input:first" ).val() === "correct") {
-    alert('Your form is going to be submitted now.');
-    return;
-  }
-}
 
 
 $(document).ready(function(){
    $('p').on('click', getIt());
    $('img').on('load', frameIt());
-   pressIt();
-   submitIt());
+
 });

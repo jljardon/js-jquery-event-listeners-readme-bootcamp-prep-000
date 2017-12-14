c99e6abcc5ad794f53f1c2af6a7857f9a7ef574f
Jose Luis Jardon <jljardon.c@gmail.com>
2017-12-14 05:27:16 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+var keyName
+
+function getIt(){
+  alert('Hey!');
+}
+
+function frameIt(){
+   $('img').addClass('tasty');
+}
+
+
+function pressIt(){
+   document.onkeydown = function(event) {
+    if(event.keyCode == 71){
+       alert('You pressed the G key!');
+    }
+   }
+}
+
+function submitIt(){
+  if ($( "input:first" ).val() === "correct") {
+    alert('Your form is going to be submitted now.');
+    return;
+  }
+}
+
 
 $(document).ready(function(){
-
-// call functions here
-
+   $('p').on('click', getIt());
+   $('img').on('load', frameIt());
+   $('#typing').on('keydown', pressIt());
+   $("form").on("submit",submitIt());
 });

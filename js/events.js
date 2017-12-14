0e20e33e80776c2dba40583b1f9d482c4f403717
Jose Luis Jardon <jljardon.c@gmail.com>
2017-12-14 05:55:45 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,9 @@
 //define functions here
 
 function getIt(){
+   $('p').on('click', function(){
   alert('Hey!');
+  });
 }
 
 function frameIt(){
@@ -11,7 +13,7 @@ function frameIt(){
 function pressIt(){
    $(document).on('keydown', function(key) {
       if(key.which == 71){
-      alert('s was pressed');
+      alert('You pressed the G key!');
       }
    });
 }

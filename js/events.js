903f8370869512e0ff3dc14e63e0f1d5d75c7ed9
Jose Luis Jardon <jljardon.c@gmail.com>
2017-12-14 05:42:00 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -33,6 +33,6 @@ function submitIt(){
 $(document).ready(function(){
    $('p').on('click', getIt());
    $('img').on('load', frameIt());
-   $('#typing').on('keydown', pressIt());
-   $("form").on("submit",submitIt());
+   pressIt();
+   submitIt();
 });
